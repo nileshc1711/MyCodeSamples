93afcdcce766f8a9aa6d3acb0175bb69c5fc4d99
nileshc1711 <nileshc1711@gmail.com>
2016-01-03 16:51:30 +0530

    Merge issues

--- ConsoleCSharpinDepth/Ch3Generics.cs
+++ ConsoleCSharpinDepth/Ch3Generics.cs
@@ -139,5 +139,67 @@ namespace ConsoleCSharpinDepth
 
     #endregion
 
+    #region Type Inference for type arguments of generic methods
+    public class CompareToDefaultValue
+    {
+
+        public static int CompareToDefault<T>(T value) where T : IComparable<T>
+        {
+            return value.CompareTo(default(T));
+        }
+
+        public static void DoDefaultComparison()
+        {
+            Console.WriteLine(CompareToDefault("X"));
+            Console.WriteLine(CompareToDefault(10));
+            Console.WriteLine(CompareToDefault(0));
+            Console.WriteLine(CompareToDefault(-10));
+            Console.WriteLine(CompareToDefault(DateTime.MinValue));
+        }
+    }
+
+    #endregion
+
+    #endregion
+
+    #region Complete Generics Implemented
+    public sealed class Pair<T1, T2> : IEquatable<Pair<T1, T2>>
+    {
+        private static readonly IEqualityComparer<T1> FirstComparer = EqualityComparer<T1>.Default;
+        private static readonly IEqualityComparer<T2> SecondComparer = EqualityComparer<T2>.Default;
+
+        private readonly T1 first;
+        private readonly T2 second;
+
+        public Pair(T1 first, T2 second) 
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public T1 First { get { return first; } }
+        public T2 Second { get { return second; } }
+
+        public bool Equals(Pair<T1, T2> other) 
+        {
+            return other != null && FirstComparer.Equals(this.First, other.First)
+                && SecondComparer.Equals(this.Second, other.Second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Pair<T1, T2>); 
+            //return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstComparer.GetHashCode(first) * 37 + SecondComparer.GetHashCode(second);
+        }
+
+
+    }
+
+
     #endregion
 }
--- ConsoleCSharpinDepth/Program.cs
+++ ConsoleCSharpinDepth/Program.cs
@@ -16,118 +16,118 @@ namespace ConsoleCSharpinDepth
 
         static void Main(string[] args)
         {
-            int i = 10;
-            double d = i;
+//            int i = 10;
+//            double d = i;
 
 
-            #region Chapter 1 - Starting- Simple DataTypes
-            var products = Product3.GetSampleProducts();
+//            #region Chapter 1 - Starting- Simple DataTypes
+//            var products = Product3.GetSampleProducts();
 
-            foreach (var product in products)
-            {
-                Console.WriteLine(product.ToString());
-            }
-
-
-
-            #region Sorting and Searching
-
-            var prodList = Product.GetSampleProducts();
-            prodList.Sort(new ProductComparer());
-
-            foreach (var product in prodList)
-            {
-                Console.WriteLine(product.ToString());
-            }
-
-
-            var prods = Product4.GetSampleProducts();
-            prods.Sort(new ProductComparer());
-            foreach (var product in prods)
-            {
-                Console.WriteLine(product);
-            }
-
-
-            #endregion
-
-            #region Querying Collections
-            var list = Product3.GetSampleProducts();
-
-            Predicate<Product3> Test = delegate(Product3 p) { return p.Price > 10m; };
-            var matches = list.FindAll(Test);
-
-            Action<Product3> print = Console.WriteLine;
-
-            matches.ForEach(print);
-
-            list.FindAll(delegate(Product3 p) { return p.Price > 10m; })
-                               .ForEach(Console.WriteLine);
-            Console.WriteLine("=====================================");
-            list.Where(p => p.Price > 10m).ToList<Product3>().ForEach(Console.WriteLine);
-            Console.WriteLine("=====================================");
-
-            list.ForEach(p =>
-            {
-                if (p.Price > 10m)
-                    Console.WriteLine(p);
-            });
-
-            var filtered = from p in list
-                           where p.Price > 10
-                           select p;
+//            foreach (var product in products)
+//            {
+//                Console.WriteLine(product.ToString());
+//            }
 
 
 
-            #endregion
+//            #region Sorting and Searching
 
-            #region Querying XML data
+//            var prodList = Product.GetSampleProducts();
+//            prodList.Sort(new ProductComparer());
 
-            Product4.QueryXMLData();
-            #endregion
-
-            #region COM Interop
-            Product4.SaveDataToExcel();
-            #endregion
-
-            #endregion
+//            foreach (var product in prodList)
+//            {
+//                Console.WriteLine(product.ToString());
+//            }
 
 
-            #region Delegate- Chapter -2
-            Person jon = new Person("jon");
-            Person tom = new Person("tom");
-            StringProcessor JonsVoice, TomsVoice, background;
-            JonsVoice = new StringProcessor(jon.Say);
-            TomsVoice = new StringProcessor(tom.Say);
-            background = new StringProcessor(Background.Note);
-            JonsVoice("Hello,Son");
-            TomsVoice.Invoke("Hello, Daddy!");
-            background("The World goes zoom..");
+//            var prods = Product4.GetSampleProducts();
+//            prods.Sort(new ProductComparer());
+//            foreach (var product in prods)
+//            {
+//                Console.WriteLine(product);
+//            }
 
 
-            #endregion
+//            #endregion
 
-            #region chapter 3 Generics
-            String text = @"Do you like green eggs and ham?
-                            I do not like them, Sam-I-am.
-                            I do not like green eggs and ham.";
+//            #region Querying Collections
+//            var list = Product3.GetSampleProducts();
 
-            var frequencies = Generics.CountWords(text);
+//            Predicate<Product3> Test = delegate(Product3 p) { return p.Price > 10m; };
+//            var matches = list.FindAll(Test);
 
-            foreach (KeyValuePair<String, Int32> entry in frequencies) {
+//            Action<Product3> print = Console.WriteLine;
 
-                String word = entry.Key;
-                Int32 count = entry.Value;
-                Console.WriteLine("{0} : {1}", word, count);
+//            matches.ForEach(print);
+
+//            list.FindAll(delegate(Product3 p) { return p.Price > 10m; })
+//                               .ForEach(Console.WriteLine);
+//            Console.WriteLine("=====================================");
+//            list.Where(p => p.Price > 10m).ToList<Product3>().ForEach(Console.WriteLine);
+//            Console.WriteLine("=====================================");
+
+//            list.ForEach(p =>
+//            {
+//                if (p.Price > 10m)
+//                    Console.WriteLine(p);
+//            });
+
+//            var filtered = from p in list
+//                           where p.Price > 10
+//                           select p;
+
+
+
+//            #endregion
+
+//            #region Querying XML data
+
+//            Product4.QueryXMLData();
+//            #endregion
+
+//            #region COM Interop
+//            Product4.SaveDataToExcel();
+//            #endregion
+
+//            #endregion
+
+
+//            #region Delegate- Chapter -2
+//            Person jon = new Person("jon");
+//            Person tom = new Person("tom");
+//            StringProcessor JonsVoice, TomsVoice, background;
+//            JonsVoice = new StringProcessor(jon.Say);
+//            TomsVoice = new StringProcessor(tom.Say);
+//            background = new StringProcessor(Background.Note);
+//            JonsVoice("Hello,Son");
+//            TomsVoice.Invoke("Hello, Daddy!");
+//            background("The World goes zoom..");
+
+
+//            #endregion
+
+//            #region chapter 3 Generics
+//            String text = @"Do you like green eggs and ham?
+//                            I do not like them, Sam-I-am.
+//                            I do not like green eggs and ham.";
+
+//            var frequencies = Generics.CountWords(text);
+
+//            foreach (KeyValuePair<String, Int32> entry in frequencies) {
+
+//                String word = entry.Key;
+//                Int32 count = entry.Value;
+//                Console.WriteLine("{0} : {1}", word, count);
             
-            }
+//            }
 
 
-            Generics.DoConversion();
-
+//            Generics.DoConversion();
+            CompareToDefaultValue.DoDefaultComparison();
             
 
-            #endregion
+            //#endregion
 
             
 
